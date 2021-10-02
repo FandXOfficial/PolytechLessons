84500dc5a4557705b246e6bea11628a80ea98d7a
FandXOfficial <90962224+FandXOfficial@users.noreply.github.com>
2021-10-02 12:27:03 +0300

    Add files via upload

---
+++ Lab №4/ConsoleApplication5/ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+    float a = 0;
+    float b = 0;
+    std::cout << "BBedite nenulevoe chislo №1 = ";
+    std::cin >> a;
+    if (a != 0)
+    {
+        std::cout << "BBedite nenulevoe chislo №2 = ";
+        std::cin >> b;
+        if (b != 0)
+        {
+            float ma = abs(a);
+            float mb = abs(b);
+            float s = ma + mb;
+            float r = ma - mb;
+            float p = ma * mb;
+            float ch = ma / mb;
+            std::cout << s << '\n' << r << '\n' << p << '\n' << ch;
+        }
+    }
+}
