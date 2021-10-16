90bd540c99979de9d7446959c4a0689f1fd23e6d
FandXOfficial <90962224+FandXOfficial@users.noreply.github.com>
2021-10-16 12:33:38 +0300

    Add files via upload

---
+++ Лабораторные ВВП/Lab №5.1/ConsoleApplication3.cpp
@@ -0,0 +1,48 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    float xA, xB, xC, lenAC, lenBC, AC_BC;
+    std::cout << "Введите координату (x) точек A и B последовательно" << "\n";
+    std::cin >> xA >> xB;
+    if (xA == xB)
+    {
+        std::cout << "Длина отрезка = 0";
+    }
+    else
+    {
+        std::cout << "Введите координату (x) точки С, лежащей между A и B" << "\n";
+        std::cin >> xC;
+        if (xA < xB)
+        {
+            if (xA < xC < xB)
+            {
+                lenAC = xC - xA;
+                lenBC = xB - xC;
+                AC_BC = lenAC * lenBC;
+                std::cout << "Произведение длин отрезков AC и BC = " << AC_BC;
+            }
+            else
+            {
+                std::cout << "Точка С лежит вне отрезка АВ";
+            }
+        }
+        if (xB < xA)
+        {
+            if (xB < xC < xA)
+            {
+                lenAC = xA - xC;
+                lenBC = xC - xB;
+                AC_BC = lenAC * lenBC;
+                std::cout << "Произведение длин отрезков AC и BC = " << AC_BC;
+            }
+            else
+            {
+                std::cout << "Точка С лежит вне отрезка АВ";
+            }
+        }
+    }
+}
+    
